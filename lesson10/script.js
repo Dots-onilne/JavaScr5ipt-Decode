1b3aa58d9695d6e10943afd2a27b3f0c2b07d3f8
Dastan <duston.92@gmail.com>
2022-06-16 20:40:25 +0600

    inital commit

---
+++ lesson10/script.js
@@ -0,0 +1,93 @@
+// 1
+
+// let x;
+// x = 15;
+// y=x+1;
+// if z > {
+//     console.log()
+// }
+
+
+// 2
+
+
+// let x = +prompt("", "")
+// if (x > 0){
+//     console.log("Polojitelnoe chislo");
+// }
+
+// else{
+//     console.log("otritsatelnoe chislo")
+// }
+
+// 3
+
+
+// let x = +prompt("","")
+// if (x>=21){
+//     console.log("Поздравляю! Вы имеете право голосовать.")
+// } else{
+//     console.log("Вы не имеете права голосовать!!!")
+// }
+
+// let x = +prompt("","")
+
+// x>=21 ?console.log("Поздравляю! Вы имеете право голосовать.") : console.log("Вы не имеете права голосовать!!!")
+
+
+
+// 4
+
+
+// let x = +prompt("","")
+// if (x<=150){
+//     console.log("Человек гном.");
+// }  else if (x<180){
+//     console.log("Нормально");
+// }else {
+//     console.log("Гигант!")
+// }
+
+
+// 5
+
+// let a = +prompt("","")
+// let b = +prompt("","")
+// let c = +prompt("","")
+
+// console.log(Math.max(a, b, c))
+
+
+// 6
+
+// let x = +prompt("Температура ","")
+// if (x<0){
+// console.log("Морозная погода");
+// } else if (x<=10){
+//     console.log("очень холодная погода")
+// } else if (x<=20){
+//     console.log("Холодная погода")
+// } else if (x<=30){
+//     console.log("Нормальная температура")
+// } else if (x<40){
+//     console.log("жарко")
+// }else {
+//     console.log("очень жарко")
+// }
+
+// 7
+
+// let a = +prompt("","")
+// let b = +prompt("","")
+// let c = +prompt("","")
+
+// let x = a+b+c
+// if (x==180){
+//     console.log("Треугольник правильный!")
+// } else {
+//     console.log("Треугольник не действителен!")
+// }
+
+// 8
+
+let x = promt("","")
---
+++ lesson9 Jc/script.js
@@ -0,0 +1,37 @@
+// let x;
+// let y;
+// let z;
+// x = 33;
+// y = 100;
+// z = x + y;
+// alert(`Сумма: ${x} + ${y} = ${z}`);
+
+// console.log("z");
+
+
+
+// let a = +prompt("Введите число", "");
+// let b = +prompt("Введите число", "");
+// let c = +prompt("Введите число", "");
+// alert(a*y*z);
+
+
+// let x = +prompt(++x);
+// let x = +prompt(y--);
+// alert(`${x}`)
+
+
+
+// let x = +prompt("","")
+// console.log(`${x} - 1 = ${x-1}`);
+// console.log(`${x} - 1 = ${x-1}`);
+
+
+let x;
+let y;
+let z;
+z = 10;
+let z = +prompt("","")
+console.log(`${
+    
+}`)
